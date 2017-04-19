89a0a94073ae371ab9bfaf2bf729a320f20f8f29
Juho Viitasalo <juho.viitasalo@lildrop.com>
2017-04-19 16:12:47 +0300

    Added Redux and example store with some functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+  }
+
+  incrementIfOdd = () => {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement()
+    }
+  }
+
+  incrementAsync = () => {
+    setTimeout(this.props.onIncrement, 1000)
+  }
   render() {
+    const { value, onIncrement, onDecrement } = this.props
     return (
       <div className="App">
         <div className="App-header">
@@ -12,6 +28,13 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Clicked: {value} times
+          <button onClick={onIncrement}>+</button>
+          <button onClick={onDecrement}>-</button>
+          <button onClick={this.incrementIfOdd}>Increment if odd</button>
+          <button onClick={this.incrementAsync}>Increment async</button>
         </p>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducers from './reducers'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
-  <App />,
+const store = createStore(reducers)
+
+const render = () => ReactDOM.render(
+  <App
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
   document.getElementById('root')
-)
+);
+
+render()
+store.subscribe(render)
